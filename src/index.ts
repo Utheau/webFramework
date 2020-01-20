ebffc4f3e52a0065627550f03529e6f9a9d7f0d7
Antonio Paolo Mecci <antoniopaolomecci@gmail.com>
2020-01-20 13:00:13 +0100

    events and sync models

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,8 @@ import { User } from './models/User';
 
 const user = new User({ name: 'yoo', age: 55 });
 
-user.save();
+user.events.on('change', () => {
+  console.log('change, uere')
+});
+
+user.events.trigger('change');
